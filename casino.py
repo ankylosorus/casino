82742cd29b4515daf706327a22da754f2a4d4876
ankylosorus <91287515+ankylosorus@users.noreply.github.com>
2022-06-20 13:29:26 +0200

    Add files via upload

--- casino.py
+++ casino.py
@@ -10,14 +10,22 @@ Example:
 
 Attributes:
     bet(int): the number between 1 and 50 on which the player bets
-    #color(string): the color associated with a given number, black for even, red for odd
-    bille(int): the winning number picked randomly between 0 and 50
+    ball(int): the winning number picked randomly between 0 and 50
     wallet(int): the money available for the player to bet, equals 100$ at the beginning of the game
     betamount(int): the money bet by the player on a round
+    history(list): the previous winning numbers
 
 """
 
 import random
+
+import time
+
+import math
+
+#import os
+
+import getpass
 
 def givecolor(number):
     """
@@ -35,27 +43,86 @@ def givecolor(number):
     else:
         return("red")
 
-
 wallet = 100
 
-bille = random.randrange(1,50)
+history = []
 
-#while wallet > 0 
+time.sleep(1)
 
-print("You have " + str(wallet) + "$")
+#print(f"\nHello {os.environ.get('USERNAME')}\n")
+print(f"\nHello {getpass.getuser()}\n")
 
-bet = int(input("Choose a number between 1 and 50 to bet on "))
+while wallet > 0:
+    #obligé de mettre variable ball dans boucle sinon ne va pas s'update
+    ball = random.randrange(1,50)
+    # math.ceil arrondit un float à l'entier supérieur
+    retry = ""
+    if history == []:
+        print(f"You have {math.ceil(wallet)}$\n")
+    else:
+        print(f"You now have {math.ceil(wallet)}$\n")
+    while retry != "y" and retry != "n":
+        if history == []:
+            retry = input("Do you want to try your luck? (y/n)\n")
+        else:
+            retry = input("Do you still want to play? (y/n)\n")
+        if retry == "n":
+            print("You don't know what you're missing... Bye")
+            exit()
+        elif retry == "y":
+            break
 
-betamount = input("How much do you want to bet on " + str(bet) + " (" + givecolor(bille) + ")" )
+    #si input n'est pas un integer --> erreur / stacktrace
+    bet = 0
+    while bet < 1 or bet > 50:
+        try:
+            bet = int(input("\nChoose a number between 1 and 50 to bet on\n"))
+        except ValueError: 
+            continue
 
-print("*Roule*")
+    betamount = 0
+    while betamount < 1 or betamount > wallet:
+        try:    
+            #j'ai essayé le f dans le input, ça marche! pourquoi bleu et pas violet?
+            betamount = int(input(f"\nHow much do you want to bet on {bet} ({givecolor(bet)})?\n"))
+            if betamount == wallet:
+                print("! ALL IN !")
+        except ValueError:
+            continue
+        if betamount > wallet:
+            print("You do not have enough in your wallet")
 
-print("The number drawn is...")
+    # print("You bet on the " + str(ball) + " " + "(" + givecolor(ball) + ")")
+    print(f"\nYou bet {betamount}$ on {bet} ({givecolor(bet)})\n")
 
-# print(bille + "(" + givecolor(bille) + ")")
-# print("{} ({})".format(bille, givecolor(bille)))
-print(f"{bille} ({givecolor(bille)})")
+    time.sleep(1)
 
-# print("You bet on the " + str(bille) + " " + "(" + givecolor(bille) + ")")
-print(f"You bet on the {bille} ({givecolor(bille)})")
+    print("*Spins*")
+
+    time.sleep(2)
+
+    print(f"The winning number is {ball} {givecolor(ball)} !")
+    
+    history.append(ball)
+
+    time.sleep(2)
+
+    if ball == bet:
+        wallet = wallet + betamount*4
+        time.sleep(1)
+        print("Well done. You win 3 times your bet\n")
+    else:
+        wallet = wallet - betamount
+        if givecolor(ball) == givecolor(bet):
+            time.sleep(1)
+            print("\n==> You missed... but you guessed the color, you win half your bet\n")
+            wallet = wallet + betamount*1.5
+        else:
+            print ("\n==> You missed\n")
+
+print("OUT!")
+exit()
+
+
+
 
